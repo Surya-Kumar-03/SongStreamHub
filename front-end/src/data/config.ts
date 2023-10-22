64b61bd48806312a9dd18ed5b74e8b8bcd7506b0
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-22 15:26:55 +0530

    Formatting Code

--- front-end/src/data/config.ts
+++ front-end/src/data/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-    links: {
-        github: "https://github.com/Surya-Kumar-03/SongStreamHub"
-    }
-} 
+	links: {
+		github: 'https://github.com/Surya-Kumar-03/SongStreamHub',
+	},
+};
